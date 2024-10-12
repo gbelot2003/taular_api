d3dbadb3832719fa11481d360a2a66f4d45c029b
Gerardo Belot <gbelot2003@hotmail.com>
2024-10-11 21:58:02 -0600

    :bug: eliminando classes_student

--- database/migrations/2024_10_12_023236_create_classes_table.php
+++ database/migrations/2024_10_12_023236_create_classes_table.php
@@ -31,6 +31,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('classes_student');
         Schema::dropIfExists('classes');
     }
 };
